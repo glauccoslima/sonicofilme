d25927105f0e731c4c5b72ce2f547a9fbcb5574d
Glaucco <glauccolima@gmail.com>
2023-06-13 22:36:26 -0300

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,35 @@
+// Importando os módulos do Gulp e as suas dependências
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 
+// Função para minificar os scripts
 function scripts() {
-    return gulp.src('./src/scripts/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('./dist/js'))
+    return gulp.src('./src/scripts/*.js') // Seleciona os arquivos JavaScript na pasta './src/scripts'
+        .pipe(uglify()) // Minifica os scripts
+        .pipe(gulp.dest('./dist/js')); // Salva os scripts minificados na pasta './dist/js'
 }
 
+// Função para compilar e minificar os estilos
 function styles() {
-    return gulp.src('./src/styles/*.scss')
-        .pipe(sass({ outputStyle: 'compressed' }))
-        .pipe(gulp.dest('./dist/css'));
+    return gulp.src('./src/styles/*.scss') // Seleciona os arquivos Sass na pasta './src/styles'
+        .pipe(sass({ outputStyle: 'compressed' })) // Compila os estilos Sass e os comprime
+        .pipe(gulp.dest('./dist/css')); // Salva os estilos compilados na pasta './dist/css'
 }
 
+// Função para otimizar as imagens
 function images() {
-    return gulp.src('./src/images/**/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('./dist/images'));
+    return gulp.src('./src/images/**/*') // Seleciona todos os arquivos de imagem na pasta './src/images' e em subpastas
+        .pipe(imagemin()) // Otimiza as imagens
+        .pipe(gulp.dest('./dist/images')); // Salva as imagens otimizadas na pasta './dist/images'
 }
 
+// Tarefa padrão do Gulp, executada ao chamar 'gulp' no terminal
 exports.default = gulp.parallel(styles, images, scripts);
 
+// Tarefa para monitorar os arquivos e executar as tarefas relacionadas quando houver alterações
 exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
-    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles)); // Monitora os arquivos Sass e executa a tarefa 'styles' quando houver alterações
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts)); // Monitora os arquivos JavaScript e executa a tarefa 'scripts' quando houver alterações
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,66 +1,94 @@
+// Este evento é disparado quando o HTML é completamente carregado e pronto
 document.addEventListener('DOMContentLoaded', function() {
+    // Seleciona todos os elementos que têm o atributo 'data-tab-button'
     const buttons = document.querySelectorAll('[data-tab-button]');
+    // Seleciona todos os elementos que têm o atributo 'data-faq-question'
     const questions = document.querySelectorAll('[data-faq-question]');
 
+    // Seleciona o elemento com a classe 'hero' e obtém a sua altura
     const heroSection = document.querySelector('.hero');
     const alturaHero = heroSection.clientHeight;
 
+    // Adiciona um ouvinte para o evento de rolagem (scroll) da janela
     window.addEventListener('scroll', function() {
+        // Obtém a posição atual de rolagem
         const posicaoAtual = window.scrollY;
 
+        // Verifica se a posição atual de rolagem é menor do que a altura da seção hero
         if (posicaoAtual < alturaHero) {
+            // Se for, oculta os elementos do cabeçalho (header)
             ocultaElementosDoHeader();
         } else {
+            // Se não for, exibe os elementos do cabeçalho
             exibeElementosDoHeader();
         }
     })
 
-    // Seção de atrações, programação das abas
+    // Para cada botão, adiciona um ouvinte para o evento de clique
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
+            // Quando um botão é clicado, obtém o valor do atributo 'data-tab-button'
             const abaAlvo = botao.target.dataset.tabButton;
+            // Seleciona o elemento com o atributo 'data-tab-id' que corresponda ao valor obtido anteriormente
             const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
+            // Esconde todas as abas
             escondeTodasAbas();
+            // Mostra a aba que corresponde ao botão clicado
             aba.classList.add('shows__list--is-active');
+            // Remove a classe ativa de todos os botões
             removeBotaoAtivo();
+            // Adiciona a classe ativa ao botão clicado
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
 
-    // Seção FAQ, accordion
+    // Para cada elemento de pergunta, adiciona um ouvinte para o evento de clique
     for (let i = 0; i < questions.length; i++) {
         questions[i].addEventListener('click', abreOuFechaResposta);
     }
 })
 
+// Adiciona a classe 'header--is-hidden' ao cabeçalho, fazendo com que ele seja ocultado
 function ocultaElementosDoHeader() {
     const header = document.querySelector('header');
     header.classList.add('header--is-hidden');
 }
 
+// Remove a classe 'header--is-hidden' do cabeçalho, fazendo com que ele seja exibido
 function exibeElementosDoHeader() {
     const header = document.querySelector('header');
     header.classList.remove('header--is-hidden');
 }
-
+// Alterna a classe 'faq__questions__item--is-open' no elemento pai da pergunta clicada
+// Isso faz com que a resposta associada seja exibida ou ocultada
 function abreOuFechaResposta(elemento) {
+    // Define a classe a ser alternada
     const classe = 'faq__questions__item--is-open';
+    // Obtém o elemento pai do elemento que foi clicado
     const elementoPai = elemento.target.parentNode;
-
+    // Alternar (adicionar se não existir, remover se existir) a classe no elemento pai
     elementoPai.classList.toggle(classe);
 }
 
+// Função para remover a classe 'shows__tabs__button--is-active' de todos os botões
+// Isso faz com que todos os botões apareçam como inativos
 function removeBotaoAtivo() {
+    // Seleciona todos os botões que têm o atributo 'data-tab-button'
     const buttons = document.querySelectorAll('[data-tab-button]');
 
+    // Para cada botão, remove a classe 'shows__tabs__button--is-active'
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].classList.remove('shows__tabs__button--is-active');
     }
 }
 
+// Função para esconder todas as abas, removendo a classe 'shows__list--is-active'
+// Isso faz com que todas as abas apareçam como inativas
 function escondeTodasAbas() {
+    // Seleciona todos os elementos que têm o atributo 'data-tab-id'
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
 
+    // Para cada aba, remove a classe 'shows__list--is-active'
     for (let i = 0; i < tabsContainer.length; i++) {
         tabsContainer[i].classList.remove('shows__list--is-active');
     }
